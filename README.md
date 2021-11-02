c66bdb986476e01721d64626d42ef5c51c8c114a
Soumyajit <soumyajitezio@gmail.com>
2021-11-02 23:37:35 +0530

    first prototype

---
+++ README.md
@@ -0,0 +1,3 @@
+# Demo Portfolio Website
+
+Note: This is just a demo. The website is not yet responsive enough to be viewed on different screens.
---
+++ script.js
@@ -0,0 +1,8 @@
+const text=baffle("#name");
+text.set({
+  characters:"▒▓< ▓/<█▓ █▒░░▒ ▒▓█ /▓░▒░ ░▒█▒ ▓▒█ <>>░ ▒<▒░",
+  speed:100
+});
+text.start();
+text.reveal(4000);
+text.stop();
